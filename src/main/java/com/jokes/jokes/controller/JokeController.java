8a9dec922ddcd92490ca16a25204eee7eb92e915
David Dedu <david.dedu@qualitestgroup.com>
2024-04-18 20:49:14 +0300

    query params validations

--- src/main/java/com/jokes/jokes/controller/JokeController.java
+++ src/main/java/com/jokes/jokes/controller/JokeController.java
@@ -2,13 +2,16 @@ package com.jokes.jokes.controller;
 
 import com.jokes.jokes.model.Joke;
 import com.jokes.jokes.service.JokeService;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class JokeController {
     private final JokeService jokeService;
@@ -19,8 +22,8 @@ public class JokeController {
 
     @GetMapping("jokes")
     public List<Joke> getJokes(@RequestParam(defaultValue = "5")
-                                 @Min(value = 1, message = "Number of jokes should be greater than 0")
-                                 @Min(value = 1, message = "You can get no more than 100 jokes at a time") Integer count) {
+                               @Min(value = 1, message = "Number of jokes should be greater than 0")
+                               @Max(value = 100, message = "You can get no more than 100 jokes at a time") Integer count) {
 
 
         return jokeService.getJokes(count);
